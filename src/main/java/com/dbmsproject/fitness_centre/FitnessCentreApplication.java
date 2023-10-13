49ebfee86874dede9c90a36b50df2352f4879147
thesri <srilu531@gmail.com>
2023-10-13 18:02:39 +0530

    Started the project

---
+++ src/main/java/com/dbmsproject/fitness_centre/FitnessCentreApplication.java
@@ -0,0 +1,13 @@
+package com.dbmsproject.fitness_centre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessCentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessCentreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbmsproject/fitness_centre/FitnessCentreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbmsproject.fitness_centre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessCentreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
